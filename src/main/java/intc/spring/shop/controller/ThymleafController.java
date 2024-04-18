b72e1bd70797c44f5a65813b10adec32b554b7d3
postwo <codo7717@naver.com>
2024-04-19 05:06:42 +0900

    파라미터전달

--- src/main/java/intc/spring/shop/controller/ThymleafController.java
+++ src/main/java/intc/spring/shop/controller/ThymleafController.java
@@ -1,16 +1,24 @@
 package intc.spring.shop.controller;
 
 import intc.spring.shop.dto.ItemDto;
+import intc.spring.shop.dto.ParamDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.awt.*;
 
 @Controller
+@Slf4j //log
+@RequestMapping("/thymleaf")
 public class ThymleafController {
 
-    @GetMapping("/thymleaf/ex1")
+    @GetMapping("/ex1")
     public String ex1(Model model){
         ItemDto itemDto = ItemDto.builder()
                 .itemNm("피파 24")
@@ -23,9 +31,20 @@ public class ThymleafController {
         return "thymeleaf/ex1";
     }
 
-    @GetMapping("/thymleaf/ex2")
+    @GetMapping("/ex2")
     public String ex2(){
         return "thymeleaf/ex2";
     }
+
+
+    @GetMapping("/ex3")
+    public String ex3(ParamDto dto,Model model) {
+        log.info("===============dt0: "+dto);
+        model.addAttribute("dto",dto);
+        return "thymeleaf/ex3";
+    }
+
+
+
 
 }
---
+++ src/main/java/intc/spring/shop/dto/ParamDto.java
@@ -0,0 +1,16 @@
+package intc.spring.shop.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class ParamDto {
+
+    private String name;
+    private int age;
+
+}
