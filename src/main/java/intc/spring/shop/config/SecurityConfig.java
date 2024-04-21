94d63b57dbf59508f7b4d026a99b7f3348c44bf3
postwo <codo7717@naver.com>
2024-04-22 00:34:08 +0900

    권한별 메뉴 보이기

--- src/main/java/intc/spring/shop/config/SecurityConfig.java
+++ src/main/java/intc/spring/shop/config/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request-> request
                         .requestMatchers("/css/**").permitAll() // 이렇게 해서 권한을 열어줘야  css가 먹는다
                         .requestMatchers("/","/member/**").permitAll() //접근하게 만들거
+                        .requestMatchers("/admin/**").hasRole("ADMIN") //admin 권한이 있으면 접속 가능 //컨트롤러에 url이 없어서 404뜨니까 후에 컨트롤 만드록 다시 403뜨는지 확인하기
                         .anyRequest().authenticated());//인증을 걸거다 //위에 권한을 열어준거 빼고는 전부다 로그인 페이지로 이동
 
         return http.build();
